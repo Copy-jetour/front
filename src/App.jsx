65ec6e0cce9ce410612ab33cb9818b815f42d79d
daria <kapoor.darya@gmai.com>
2024-12-23 16:23:05 +0300

    Add modal window

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import HomePage from './pages/HomePage/HomePage.jsx';
@@ -7,22 +7,36 @@ import Widget from './components/Widget/Widget.jsx';
 import CreditWidget from './components/CreditWidget/CreditWidget.jsx';
 import AdvantagesWidget from './components/AdvantagesWidget/AdvantagesWidget.jsx';
 import MapWidget from './components/MapWidget/MapWidget.jsx';
-import './styles/main.scss';
-//import './styles/index_style.css';
+import Modal from './components/Modal/Modal.jsx';
+import './App.scss';
 
 function App() {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [modalParameter, setModalParameter] = useState(null);
+
+  const openModal = (parameter) => {
+    setModalParameter(parameter);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setModalParameter(null);
+  };
+
   return (
     <div className="App">
-      <Header />
+      <Header openModal={openModal} />
       <main>
-        <JetourBanner />
-        <Widget />
-        <HomePage />
-        <CreditWidget />
-        <AdvantagesWidget />
-        <MapWidget />
+        <JetourBanner openModal={openModal} />
+        <Widget openModal={openModal} />
+        <HomePage openModal={openModal} />
+        <CreditWidget openModal={openModal} />
+        <AdvantagesWidget openModal={openModal} />
+        <MapWidget openModal={openModal} />
         <Footer />
       </main>
+      <Modal isOpen={isModalOpen} onClose={closeModal} parameter={modalParameter} />
     </div>
   );
 }
